505ef7ee8150731362e1566fad3ccbd88c361d22
Gyorgio88 <gyorgio88@gmail.com>
2020-05-15 21:31:07 +0200

    ability to create nested queries

--- lib/src/database/connection.dart
+++ lib/src/database/connection.dart
@@ -1,6 +1,6 @@
+
 import 'dart:async';
 import 'dart:io';
-import 'package:database_manager/src/orm/query/query_builder.dart';
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
